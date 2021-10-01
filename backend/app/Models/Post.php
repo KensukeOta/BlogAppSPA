50ab145415002951e8d9e02f43fdb24aa6b221cd
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2021-10-01 13:02:53 +0900

    UserモデルとPostモデル間のリレーションを記述

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -14,4 +14,10 @@ class Post extends Model
         'body',
         'user_id',
     ];
+
+    //  $post->user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
